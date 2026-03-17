af9072a0f0c464a90ec5ef08e8b0867d90cac7e6
Md Rubel Ahmed <mdrubelahmed3965@gmail.com>
2026-03-17 18:04:25 +0600

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Navigate } from 'react-router'
 import { Navigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext.jsx'
 const PrivateRoute = ({children}) => {
     const {user} = useAuth()
- return user ? children : <Navigate t0 ="/login"/>
+ return user ? children : <Navigate to ="/login"/>
 }
 
 export default PrivateRoute
